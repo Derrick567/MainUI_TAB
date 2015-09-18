d01f79543e37cf8d2a94fc4e47f68ac09cb28726
Derrick <pomelo4068@gmail.com>
2015-09-18 21:22:54 +0800

    modify mainactivity

--- Tab02/app/src/main/java/com/derrick/user/tab02/MainActivity.java
+++ Tab02/app/src/main/java/com/derrick/user/tab02/MainActivity.java
@@ -4,6 +4,7 @@ package com.derrick.user.tab02;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.Window;
@@ -64,7 +65,63 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
 
     private void setSelect(int i) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        hideFragment(transaction);
+        // 參芞扢离峈謠腔
+        // 扢离囀郖
+        switch (i)
+        {
+            case 0:
+                if (mTab01 == null)
+                {
+                    mTab01 = new WexinFragment();
+                    transaction.add(R.id.id_content, mTab01);
+                } else
+                {
+                    transaction.show(mTab01);
+                }
+                mImgWeixin.setImageResource(R.mipmap.tab_weixin_pressed);
+                break;
+            case 1:
+                if (mTab02 == null)
+                {
+                    mTab02 = new FriendFragment();transaction.add(R.id.id_content, mTab02);
+                } else
+                {
+                    transaction.show(mTab02);
 
+                }
+                mImgFrd.setImageResource(R.mipmap.tab_find_frd_pressed);
+                break;
+            case 2:
+                if (mTab03 == null)
+                {
+                    mTab03 = new AddressFragment();
+                    transaction.add(R.id.id_content, mTab03);
+                } else
+                {
+                    transaction.show(mTab03);
+                }
+                mImgAddress.setImageResource(R.mipmap.tab_address_pressed);
+                break;
+            case 3:
+                if (mTab04 == null)
+                {
+                    mTab04 = new SettingFragment();
+                    transaction.add(R.id.id_content, mTab04);
+                } else
+                {
+                    transaction.show(mTab04);
+                }
+                mImgSetting.setImageResource(R.mipmap.tab_settings_pressed);
+                break;
+
+            default:
+                break;
+        }
+
+        transaction.commit();
     }
 
     private void hideFragment(FragmentTransaction transaction) {
